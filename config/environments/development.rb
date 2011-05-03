81298640f1c882f62e81e6358ec1b2c05ae25bac
liishygo <liishygo@gmail.com>
2011-05-03 08:53:41 +0800

    add 'config.active_record.observers'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,6 @@ ForumLesson::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  config.active_record.observers = :user_observer
 end
 

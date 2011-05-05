43b69a45661bd981c98a353ebe0fccf4d8d7ed69
liishygo <liishygo@gmail.com>
2011-05-05 23:53:37 +0800

    add before_filter authenticate_user

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,6 +1,7 @@
 class ForumsController < ApplicationController
   
-  before_filter :find_forum, :only => [ :show, :edit, :update, :destroy ] 
+  before_filter :find_forum, :only => [ :show, :edit, :update, :destroy ]
+  before_filter :authenticate_user!, :only => [ :new, :create, :edit, :update, :destroy ]
   
   def index
     @forums = Forum.all

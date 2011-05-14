46229aef0069dfe0754c91c23df5938aee98a50a
liishygo <liishygo@gmail.com>
2011-05-14 21:27:35 +0800

    add 'post belong to User'

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :content
-  validates_presence_of :author
   validates_presence_of :forum_id
+  validates_presence_of :user_id
   
   belongs_to :forum
   belongs_to :user
